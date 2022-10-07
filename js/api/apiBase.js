04968450a78047d2ca60f9590e5ca8264f16d230
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-10-07 02:36:43 +0200

    added api base

--- js/api/apiBase.js
+++ js/api/apiBase.js
@@ -1,4 +1,9 @@
 export const apiUrl = `https://nf-api.onrender.com/`;
-export const apiGetProfile = `api/v1/social/profiles`;
 export const apiGetPosts = `api/v1/social/posts/`;
-export const apiSpecificPost = `api/v1/social/posts/`;
+export const apiSpecificPost = `api/v1/social/posts/` + id;
+export const apiGetProfile = `api/v1/social/profiles`;
+
+export const author = `_author`;
+export const comments = `_comments`;
+export const reactions = `_reactions`;
+export const tru = `=true`;
