b7a390cea3264a8278fd5b7c1c574750964da2dd
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-10-10 01:36:23 +0200

    Added error message

---
+++ js/components/message.js
@@ -0,0 +1,4 @@
+function message(messageType = "success", message = "") {
+  return `<div class="error-card col-1 border border-danger rounded-1 text-center"><p>Could not find this post!!!</p>
+    </div>`;
+}
