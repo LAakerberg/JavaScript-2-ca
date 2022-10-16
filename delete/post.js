678bfdca7cb28d0a2fee7ac60092fb83739d14a6
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-10-16 21:23:15 +0200

    Removed files that's not in use

--- delete/post.js
+++
@@ -1,8 +0,0 @@
-import { requestPost } from '../js/api/posts/getPosts.js';
-
-import { apiUrl } from './apiBase.js';
-import { apiGetPosts } from './apiBase.js';
-import { sortCreatedDesc } from './apiBase.js';
-import { sortCreatedAsc } from './apiBase.js';
-
-requestPost(`${apiUrl}${apiGetPosts}${sortCreatedDesc}&_author=true&limit=200`);
